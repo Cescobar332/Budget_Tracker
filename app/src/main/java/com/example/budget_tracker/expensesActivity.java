5305d38de0ea0cfc1e5bc47f5c8bca384a60f438
newMoonlight11 <104524751+newMoonlight11@users.noreply.github.com>
2023-04-21 18:09:00 -0500

    Design incomesActivity/savingsActivity/expensesActivity (CAMILA)

--- app/src/main/java/com/example/budget_tracker/expensesActivity.java
+++ app/src/main/java/com/example/budget_tracker/expensesActivity.java
@@ -3,6 +3,15 @@ package com.example.budget_tracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class expensesActivity extends AppCompatActivity {
 
@@ -10,5 +19,32 @@ public class expensesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenses);
+        TextView tv_filter1 = findViewById(R.id.tv_filter11);
+        TextView tv_filter2 = findViewById(R.id.tv_filter22);
+        TextView tv_filter3 = findViewById(R.id.tv_filter33);
+
+        Spinner mySpinner2 = findViewById(R.id.my_spinner_expenses);
+        String[] filterArray = getResources().getStringArray(R.array.filter_array);
+        List<String> filter = new ArrayList<>(Arrays.asList(filterArray));
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(this, R.layout.custom_spinner_item2, filter);
+        adapter2.setDropDownViewResource(R.layout.custom_spinner_dropdown_item);
+        mySpinner2.setAdapter(adapter2);
+
+        if (filter.size() >= 3) {
+            tv_filter1.setText(filter.get(1));
+            tv_filter2.setText(filter.get(2));
+            tv_filter3.setText(filter.get(3));}
+
+        mySpinner2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedOption = filter.get(position);
+
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Lógica para cuando no se ha seleccionado ninguna opción del spinner
+            }
+        });
     }
 }
