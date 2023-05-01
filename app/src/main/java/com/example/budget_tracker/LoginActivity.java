2d8c14cc991cf789c1d7565be9457b0943373b9a
newMoonlight11 <104524751+newMoonlight11@users.noreply.github.com>
2023-04-30 21:05:05 -0500

    Firebase conection / responsive screens Login/Register (CAMILA)

--- app/src/main/java/com/example/budget_tracker/LoginActivity.java
+++ app/src/main/java/com/example/budget_tracker/LoginActivity.java
@@ -1,14 +1,82 @@
 package com.example.budget_tracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUser, etPassword;
+    private SharedPreferences misPreferencias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        referenciar();
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        misPreferencias = getSharedPreferences("tienda_app", MODE_PRIVATE);
+        //verificar sí está logueado
+        if (misPreferencias.getBoolean("logueado", false)) {
+            Intent myIntent = new Intent(this, MainActivity.class);
+            startActivity(myIntent);
+            finish();
+        }
+    }
+
+/*    public void cargarData() {
+
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore.collection("usuarios").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DocumentSnapshot document : task.getResult()) {
+
+                        Usuario user = document.toObject(Usuario.class);
+                        user.setId(document.getId());
+                    }
+                } else {
+                    Toast.makeText(LoginActivity.this, "No se pudo conectar al servidor", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }*/
+    private void referenciar() {
+        etUser = findViewById(R.id.et_username_login);
+        etPassword = findViewById(R.id.et_password_login);
+    }
+
+    public void clickIniciarSesion(View view) {
+        String PASS = "123456";
+        String USER = "Camila";
+        String passUser = etPassword.getText().toString();
+        String userUSer = etUser.getText().toString();
+        if (PASS.equals(passUser) && USER.equals(userUSer)) {
+            SharedPreferences.Editor myEditor = misPreferencias.edit();
+            Intent myIntent = new Intent(this, MainActivity.class);
+            startActivity(myIntent);
+            finish();
+            myEditor.putBoolean("logueado", true);
+            myEditor.apply();
+        } else {
+            Toast.makeText(this, "Credenciales incorrectas", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void clickRegister (View view){
+        startActivity(new Intent(this, RegisterActivity.class));
     }
 }
--- app/src/main/java/com/example/budget_tracker/Usuario.java
+++ app/src/main/java/com/example/budget_tracker/Usuario.java
@@ -1,5 +1,47 @@
 package com.example.budget_tracker;
 
+import com.google.firebase.firestore.Exclude;
+
 public class Usuario {
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    private String id;
+    private String user;
+    private String password;
+    private String email;
+
+    public Usuario (){
+
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
