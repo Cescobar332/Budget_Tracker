5434d7cac879f89e0a2589b4267744f3d7b27c59
newMoonlight11 <104524751+newMoonlight11@users.noreply.github.com>
2023-05-12 19:02:42 -0500

    LoginActivity (CAMILA)

--- app/src/main/java/com/example/budget_tracker/RegisterActivity.java
+++ app/src/main/java/com/example/budget_tracker/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.example.budget_tracker;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -10,13 +11,18 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText etUser, etPassword, etEmail;
-
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +31,7 @@ public class RegisterActivity extends AppCompatActivity {
         etUser = findViewById(R.id.et_username_register);
         etPassword = findViewById(R.id.et_password_register);
         etEmail = findViewById(R.id.et_email_register);
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void clickLogin(View view) {
@@ -33,17 +40,22 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void clickGuardar(View view) {
         String user = etUser.getText().toString();
-        String password = etPassword.getText().toString();
         String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
 
-        Usuario newUser = new Usuario();
-        newUser.setUsuario(user);
-        newUser.setContrasena(password);
-        newUser.setCorreo(email);
-
-        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-        firestore.collection("usuarios").add(newUser);
-        Toast.makeText(this, "Se creó el nuevo usuario", Toast.LENGTH_SHORT).show();
-        finish();
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(RegisterActivity.this, "Se ha creado el usuario", Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(RegisterActivity.this, "No se ha podido crear el usuario", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
